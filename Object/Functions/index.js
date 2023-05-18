5d2a4b02417b14f3c1575f1ff3efb98a231de26f
Erick El nino <erick.tshimbombo@yahoo.com>
2023-05-18 05:47:08 +0200

    update

--- Object/Functions/index.js
+++ Object/Functions/index.js
@@ -1,1 +1,81 @@
 console.log("Create a functions");
+
+function grumpus()
+{
+    console.log("Hello world");
+    console.log("How are you doing ?");
+    console.log("We doing very well");
+}
+
+
+grumpus();
+
+
+const manoey = [1,2,3,4,8].indexOf(3);
+
+console.log(manoey);
+
+
+
+
+
+function monica(namne,surname)
+{
+    console.log(`name: ${namne} \n Surname:${surname}`);
+
+    return{
+        name: namne,
+        surname: surname
+    }
+}
+
+const data = monica('erick','elnino');
+
+console.log(data);
+
+
+
+function isValidPassword(username,password)
+{
+
+     if(password.length >= 8)
+     {
+        const valid = /\s/.test(password);
+        if(!valid)
+        {
+            const nameisyou = password.includes(username);
+
+            if(!nameisyou)
+            {
+                console.log("We good ")
+            }else
+            {
+                console.log("please make sure that your password doesnt contains username");
+            }
+
+        }else
+        {
+            console.log("Please white space was spotted");
+        }
+
+     }else
+     {
+        console.log("Please passoerd not long .should have at least 8");
+     }
+}
+
+isValidPassword("Erick","#kelnino1");
+isValidPassword("dogluvr","dogluvr123")
+
+
+
+
+
+
+
+
+
+
+
+
+
